bca418223752b51474192a8ca9d13fbf237a0e50
Prashant Sachan <swepra04@gmail.com>
2023-07-15 17:38:49 +0530

    hooks Added

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import img1 from "../Images/Screenshot 2023-07-07 100213.jpg"
+function Home() {
+    return (
+        <div>
+            <img src={img1} />
+        </div>
+    )
+}
+
+export default Home
